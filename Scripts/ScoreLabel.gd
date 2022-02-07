d40701035fec7d72ac146342a3fb91963e452e3d
eggsnham07 <eggsnham07@gmail.com>
2022-02-07 10:10:02 -0700

    Update ScoreLabel.gd

--- Scripts/ScoreLabel.gd
+++ Scripts/ScoreLabel.gd
@@ -1,7 +1,12 @@
 extends Label
 
 var score = 0
+var count = 0
 
 func add_score():
-	score += 1
-	text = str(score)
+	if count < 3:
+		count += 1
+	elif count == 3:
+		score += 1
+		text = str(score)
+		count = 0
